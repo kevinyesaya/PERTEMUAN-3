e555ba6367cdb5574e6e604373b62774e67b0da6
kevinyesaya <kevibyesaya@gmail.com>
2024-10-30 21:16:45 +0700

    Add files via upload

---
+++ array_asosiatif.php
@@ -0,0 +1,4 @@
+<?php
+$siswa = array("nama" => "Husni", "umur" => 25, "kota" => "Binjai");
+echo $siswa["nama"]; //Output: Husni
+?>
---
+++ parameter.php
@@ -0,0 +1,8 @@
+<?php
+function tambah($a, $b){
+    $hasil = $a + $b;
+    echo $hasil;
+}
+
+tambah(2, 3); //Memanggil fungsi tambah dengan parameter 2 dan 3
+?>
